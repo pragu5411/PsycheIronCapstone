5bb69f851839e89ed062a9bace9f6e78ecd3608d
Slivmus <jose.edwin997@gmail.com>
2019-03-06 11:25:05 -0700

    installed Virtual Environment

--- Python GUI/psyche_app.py
+++ Python GUI/psyche_app.py
@@ -1,14 +1,17 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.label import Label
+from  kivy.uix.filechooser import FileChooserListView
 
 
 
 
-class PongApp(App):
+class root(Widget):
+    pass
+class MainviewApp(App):
     def build(self):
-        return Label(text = "Hello World")
+        return root()
 
 
 if __name__ == '__main__':
-    PongApp().run()
+   MainviewApp().run()
