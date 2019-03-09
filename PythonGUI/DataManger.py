8f16f6deb2a72c9d040c55a13a67278e6d693c62
Slivmus <jose.edwin997@gmail.com>
2019-03-09 12:08:02 -0700

    slightly modified the Datamanger and started on Terminal

--- PythonGUI/DataManger.py
+++ PythonGUI/DataManger.py
@@ -1,29 +1,30 @@
 import os
 
-class DataManger():
-       
+class DataManger():       
 
     def __init__(self):
-        self.datasets = {} 
+        self._datasets = {} 
 
     
     def update(self):
-        self.datasets = {}
+        self._datasets = {}
         data_folder = os.path.join(os.path.dirname(__file__),"Data")
         list_of_folders = os.listdir(data_folder)
         for folder in list_of_folders:
             file_list = os.listdir(os.path.join(data_folder,folder))
             for files in file_list:
                 if(os.path.splitext(files)[1] == ".tm"):
-                    self.datasets[folder] = os.path.join(data_folder,folder,files)   
+                    self._datasets[folder] = os.path.join(data_folder,folder,files)   
     
-    def getdatasets(self):
-        return self.datasets
+    @property
+    def datasets(self):
+        return self._datasets
 
-data_list = DataManger()
-data_list.update()
-print(data_list.datasets)
-            
-    
+    @datasets.setter
+    def datasets(self,_):
+        pass
+
+
+
 
     
