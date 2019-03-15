ddf464e01c4176a3fa10a068c16c312a2e3cb54d
Slivmus <jose.edwin997@gmail.com>
2019-03-14 18:03:53 -0700

    The export button is somewhat working

---
+++ PythonGUI/Data/Cassini/Meta.tm
@@ -0,0 +1,13 @@
+
+   \begindata
+   KERNELS_TO_LOAD=(
+   'c:\Dev\PsycheIronCapstone\PythonGUI\Data\Cassini\naif0009.tls',
+   'c:\Dev\PsycheIronCapstone\PythonGUI\Data\Cassini\cas00084.tsc',
+   'c:\Dev\PsycheIronCapstone\PythonGUI\Data\Cassini\cpck05Mar2004.tpc',
+   'c:\Dev\PsycheIronCapstone\PythonGUI\Data\Cassini\020514_SE_SAT105.bsp',
+   'c:\Dev\PsycheIronCapstone\PythonGUI\Data\Cassini\981005_PLTEPH-DE405S.bsp',
+   'c:\Dev\PsycheIronCapstone\PythonGUI\Data\Cassini\030201AP_SK_SM546_T45.bsp',
+   'c:\Dev\PsycheIronCapstone\PythonGUI\Data\Cassini\04135_04171pc_psiv2.bc',
+   'c:\Dev\PsycheIronCapstone\PythonGUI\Data\Cassini\cas_v37.tf',
+   'c:\Dev\PsycheIronCapstone\PythonGUI\Data\Cassini\cas_iss_v09.ti')
+   \begintext
--- PythonGUI/psyche_app.py
+++ PythonGUI/psyche_app.py
@@ -1,6 +1,10 @@
 import tkinter as tk
 import spiceypy as spice
 from DataManger import DataManger as DM
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
 HEIGHT = 800
@@ -79,12 +83,14 @@ class psyche_app:
 
         self.observer_label = tk.Label(master= self.right_information, text = "Observer: ", bg = BUTTONCOLOR, fg = FONTCOLOR)
         self.observer_entry = tk.Entry(master= self.right_information)
-        self.observer_label.grid(row = 6 , column = 0, padx = 45, pady = 15)
-        self.observer_entry.grid(row = 6 , column = 1, padx = 45, pady = 15)
+        self.observer_label.grid(row = 7 , column = 0, padx = 45, pady = 15)
+        self.observer_entry.grid(row = 7 , column = 1, padx = 45, pady = 15)
+
+        
 
 
         self.export_menu = tk.Frame(master= self.right_side, bg = FRAMECOLOR)
-        self.export_kernal = tk.Button(master=self.export_menu, text = "Export Kernel", font = FONT,bg = BUTTONCOLOR, fg = FONTCOLOR)
+        self.export_kernal = tk.Button(master=self.export_menu, text = "Export Kernel", font = FONT,bg = BUTTONCOLOR, fg = FONTCOLOR, command= self.export_event)
         self.export_kernal.grid(row= 0, columnspan = 3, padx = 125, pady = 5)
         self.export_menu.place(relx=0,rely=.7,relwidth = 1, relheight =.3)
 
@@ -97,8 +103,66 @@ class psyche_app:
         for kernel_title in list_of_kernel.keys():
             self.kernel_list.insert(tk.END, kernel_title)
 
-    def export_event():
-        pass
+    # *** PlaceHolder ***
+    def export_event(self,*args,**kwargs):
+        time_one = self.time_one_entry.get()
+        time_two = self.time_two_entry.get()
+        time_step = int(self.time_step_entry.get())
+        reference = self.reference_entry.get()
+        abcorr = self.abberration_corr_entry.get()
+        observer = self.observer_entry.get()
+        target = self.target_entry.get()
+        
+        spice.furnsh(self.data_sets.datasets[self.kernel_list.get(tk.ACTIVE)])
+        et_times = [spice.str2et(time_one), spice.str2et(time_two)]         
+        times = [x*(et_times[1]-et_times[0])/time_step + et_times[0] for x in range(time_step)]
+        pos, dist = spice.spkpos(observer, times, reference, abcorr, target)
+        content_folder = os.path.join(os.path.dirname(__file__),self.kernel_list.get(tk.ACTIVE))
+        try:
+            os.makedirs(content_folder)
+        except FileExistsError:
+            pass
+        pos_file_path = os.path.join(content_folder,"pos.txt")
+        dist_file_path = os.path.join(content_folder,"dist.txt")
+        with open(pos_file_path,"w") as pos_file:
+            pos_file.write(str(pos))
+        with open(dist_file_path,"w") as dist_file:
+            dist_file.write(str(dist))
+
+        pos = np.asarray(pos).T 
+        fig = plt.figure(figsize=(9, 9))
+        ax  = fig.add_subplot(111, projection='3d')
+        ax.plot(pos[0], pos[1], pos[2])
+        plt.title(f'SpiceyPy {target} Position from {time_one} to {time_two}')
+            
+        pos_graph_file_path = os.path.join(content_folder,"posgraph.pdf")
+        plt.savefig(pos_graph_file_path, dpi=None, facecolor='w', edgecolor='w',
+        orientation='portrait', papertype='letter', format='pdf',
+        transparent=False, bbox_inches=None, pad_inches=0.1,
+        frameon=None, metadata=None)
+            
+        dist = np.asarray(dist)
+        print(dist) 
+        fig2 = plt.figure(figsize=(9, 9))
+        ax2  = fig2.add_subplot(111, projection='3d')
+        ax2.plot(times,dist)
+        plt.title(f'SpiceyPy {target} distance from {observer} taken from {time_one} to {time_two}')
+
+        dist_graph_file_path = os.path.join(content_folder,"distgraph.pdf")
+        plt.savefig(dist_graph_file_path, dpi=None, facecolor='w', edgecolor='w',
+        orientation='portrait', papertype='letter', format='pdf',
+        transparent=False, bbox_inches=None, pad_inches=0.1,
+        frameon=None, metadata=None)
+    
+        
+
+
+
+
+        
+        
+
+        
     def import_event():
         pass
     def delete_event():
